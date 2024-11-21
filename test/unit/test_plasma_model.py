a6ca2585175e8f5e2928b1a8b756602ac57f966d
Thomas Wilson <thomas.wilson@ukaea.uk>
2024-11-21 00:24:57 +0000

    Add skeleton for plasma model unit test

---
+++ test/unit/test_plasma_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# Standard imports
+import logging
+import numpy as np
+import pytest
+
+# Local imports
+
+logger = logging.getLogger(__name__)
+
+class TestC2Ramp:
+    pass
+
+class TestQuadraticWell:
+    pass
