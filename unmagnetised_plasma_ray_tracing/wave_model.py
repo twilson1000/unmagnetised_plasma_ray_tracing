bdd3ec8d491f2a962e189ebf747ee4df64d43bd1
Thomas Wilson <thomas.wilson@ukaea.uk>
2024-11-21 00:54:17 +0000

    Add skeleton for wave model

---
+++ unmagnetised_plasma_ray_tracing/wave_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Standard imports
+import logging
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Local imports
+
+logger = logging.getLogger(__name__)
+
+class WaveModel:
+    pass
