fa3b2c1ac721646f379ced25d910920d1d94ebc6
Thomas Wilson <thomas.wilson@ukaea.uk>
2024-11-23 01:15:21 +0000

    Remove __hash__

--- unmagnetised_plasma_ray_tracing/parameter.py
+++ unmagnetised_plasma_ray_tracing/parameter.py
@@ -45,9 +45,6 @@ class Parameter:
     
     def __repr__(self):
         return f"Parameter.{self.name}"
-
-    def __hash__(self):
-        return hash(str(self))
     
     def value_shape(self, dimension: int) -> Tuple[int]:
         '''
